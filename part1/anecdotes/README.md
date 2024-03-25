541f37edc8715504fcecc2935959601b5496723e
Rodrigo <rodrigo@asmalljob.com>
2024-03-25 12:56:05 +0100

    part1 done

--- part1/anecdotes/README.md
+++ part1/anecdotes/README.md
@@ -1,17 +1,36 @@
-### 1.12*: anecdotes, paso 1
+### 1.12\*: anecdotes, paso 1
+
 El mundo de la ingeniería de software está lleno de anécdotas que destilan verdades atemporales de nuestro campo en breves frases.
 
 Expande la siguiente aplicación agregando un botón en el que se pueda hacer clic para mostrar una anécdota aleatoria del campo de la ingeniería de software:
 
 ```javascript
 const anecdotes = [
-    'If it hurts, do it more often.',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
-    'The only way to go fast, is to go well.'
-  ]
+	'If it hurts, do it more often.',
+	'Adding manpower to a late software project makes it later!',
+	'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+	'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+	'Premature optimization is the root of all evil.',
+	'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+	'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+	'The only way to go fast, is to go well.',
+];
 ```
+
+Tu aplicación finalizada podría verse así
+
+![view](https://fullstackopen.com/static/8577fa00fc4d946e2322de9b2707c89c/5a190/18a.png)
+
+### 1.13\*: anecdotes, paso 2
+Expande tu aplicación para que puedas votar por la anécdota mostrada.
+
+![example](https://fullstackopen.com/static/06f95cb43a18bd6429174200a8d17cff/5a190/19a.png)
+
+Nota: almacena los votos de cada anécdota en una matriz u objeto en el estado del componente. Recuerda que la forma correcta de actualizar el estado almacenado en estructuras de datos complejas como objetos y matrices es hacer una copia del estado.
+
+### 1.14\*: anecdotes, paso 3
+Ahora implementa la versión final de la aplicación que muestra la anécdota con el mayor número de votos
+
+![referencia](https://fullstackopen.com/static/3e8638efbbbbcabac7bb79466ab3a5f6/5a190/20a.png)
+
+Si se empatan varias anécdotas en el primer lugar, es suficiente con solo mostrar una de ellas.
--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -1,35 +1,73 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+	const anecdotes = [
+		'If it hurts, do it more often.',
+		'Adding manpower to a late software project makes it later!',
+		'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+		'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+		'Premature optimization is the root of all evil.',
+		'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+		'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+		'The only way to go fast, is to go well.',
+	];
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	const [selected, setSelected] = useState(0);
+
+	//votar por anecdota
+	const INITIAL_VOTES = { 0: 0 };
+	const [votes, setVotes] = useState(INITIAL_VOTES);
+
+	const voting = () => {
+		// Copia el objeto anterior y añade la puntuación al atributo
+		const newVote = { ...votes, [selected]: votes[selected] ? votes[selected] + 1 : 1 };
+
+		return setVotes(newVote);
+	};
+
+	// Renderizar el total de votos por anecdotas
+	const totalVote = votes[selected] ? votes[selected] : 0;
+
+	// Muestra la anécdota con el mayor número de votos
+	let MOST_VOTED = [];
+	const votados = Object.values(votes);
+	const numeroMasAlto = Math.max(...votados);
+
+  //recorremos el objeto para relacionar clave y valor al más votado
+	for (const [key, value] of Object.entries(votes)) {
+		if (value === numeroMasAlto) {
+			MOST_VOTED = MOST_VOTED.concat(key);
+		}
+	}
+
+  //Renderiza las anecdotas más votadas, si hay mas de una
+	const anecdoteMostVotes = MOST_VOTED.map( a => {
+    return <p key={a}>"{anecdotes[a]}"</p>
+  });
+
+
+	const nextAnecdote = () => {
+		// Generar un número aleatorio entre 0 y el maximo de elementos de una array
+		const numeroAleatorio = Math.random() * anecdotes.length;
+		const entero = Math.floor(numeroAleatorio);
+		return setSelected(entero);
+	};
+
+	return (
+		<main>
+			<section>
+				<h1>Anecdote of the day</h1>
+				<p>"{anecdotes[selected]}"</p>
+				<p>Has {totalVote} votes</p>
+				<button onClick={voting}>Vote</button>
+				<button onClick={nextAnecdote}>Next anecdote</button>
+			</section>
+			<section>
+				<h2>Anecdote with most votes</h2>
+        {anecdoteMostVotes}
+			</section>
+		</main>
+	);
 }
 
-export default App
+export default App;
