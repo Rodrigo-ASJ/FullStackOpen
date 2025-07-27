41ca177e0f4e8520e27136bad497c17b8de4fe64
Rodrigo <rodrigo@asmalljob.com>
2025-07-27 10:51:36 +0200

    part4 notas MVC

---
+++ part3/backend/app.js
@@ -0,0 +1,43 @@
+// Express
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+// Configuración y utilidades
+const config = require('./utils/config')
+const logger = require('./utils/loggers');
+const middleware = require('./utils/middleware');
+
+// Controllers
+const notesRouter = require('./controllers/notes');
+
+// Mongoose para la conexión a MongoDB
+const mongoose = require('mongoose');
+
+mongoose.set('strictQuery', false);
+
+logger.info('connecting to', config.MONGODB_URI);
+
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connecting to MongoDB:', error.message)
+  })
+
+app.use(cors())
+// middleware para servir archivos estáticos
+app.use(express.static('dist'))
+//json-parser 
+app.use(express.json())
+// middleware para registrar solicitudes
+app.use(middleware.requestLogger)
+// middleware para manejar solicitudes de notas
+app.use('/api/notes', notesRouter)
+// controlador de solicitudes con endpoint desconocido
+app.use(middleware.unknownEndpoint)
+//este debe ser el último middleware cargado, ¡también todas las rutas deben ser registrada antes que esto!
+app.use(middleware.errorHandler)
+
+module.exports = app;
--- part3/backend/models/note.js
+++ part3/backend/models/note.js
@@ -1,13 +1,4 @@
 const mongoose = require('mongoose');
-mongoose.set('strictQuery',false);
-
-const url = process.env.MONGODB_URI;
-
-console.log('connecting to', url);
-
-mongoose.connect(url)
-    .then( result => { console.log( "connected to MongoDB" )})
-    .catch( error => { console.log("error connecting to MongoDB:", error.message)});
 
 const noteSchema = new mongoose.Schema({ 
     content:{ 
@@ -27,6 +18,6 @@ noteSchema.set('toJSON', {
     }
 });
 
-const Note = mongoose.model( 'Note', noteSchema);
+const Note = mongoose.model('Note', noteSchema);
 
 module.exports = Note;
